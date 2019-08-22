f87568d4b7997cb2a730a0d421b6d14c2bc6a39d
squirmlabs <squirmlabs@gmail.com>
2019-08-21 18:33:41 -0700

    Setup data methods

--- index.js
+++ index.js
@@ -13,9 +13,34 @@ const app = express();
 // Creating our GraphQL Schema
 const schema = buildSchema(`
   type Query {
-    message: String
+    contact(id: Int!): Contact
+    contacts(name: String): [Contact]
+  }
+
+  type Contact {
+    id: Int
+    name: String
+    phone: String
+    email: String
   }
 `);
+
+// Data methods
+const methods = {
+  getContact: args => {
+    const { id } = args;
+    return contacts.filter(contact => contact.id === id)[0];
+  },
+  getContacts: args => {
+    const { name = false } = args;
+    // If we don't get a name we return all contacts
+    if (!name) {
+      return contacts;
+    }
+    // Returning contacts with same name...
+    return contacts.filter(contact => contact.name.includes(name));
+  }
+};
 
 // Root has the methods we will execute to get the data
 const root = {
