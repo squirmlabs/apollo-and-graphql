f4bfbc30d389dd7374abb951e5db41cf0fae8435
squirmlabs <squirmlabs@gmail.com>
2019-08-23 09:06:32 -0700

    GraphQl queries, fragments, mutations and demonstrations

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ const schema = buildSchema(`
   type Query {
     contact(id: Int!): Contact
     contacts(name: String): [Contact]
+    hello: String
+  }
+  
+  type Mutation {
+    updateContact (
+      id: Int!,
+      name: String!,
+      phone: String!,
+      email: String!
+    ) : Contact
   }
 
   type Contact {
@@ -42,13 +52,31 @@ const methods = {
     }
     // Returning contacts with same name...
     return contacts.filter(contact => contact.name.includes(name));
+  },
+  updateContact: ({ id, name, phone, email }) => {
+    contacts.forEach(contact => {
+      if (contact.id === id) {
+        // Updating only the fields that has new values...
+        contact.name = name || contact.name;
+        contact.phone = phone || contact.phone;
+        contact.email = email || contact.email;
+      }
+    });
+
+    return contacts.filter(contact => contact.id === id)[0];
+  },
+
+  hello: () => {
+    return 'Hello world!';
   }
 };
 
 // Root has the methods we will execute to get the data
 const root = {
   contact: methods.getContact,
-  contacts: methods.getContacts
+  contacts: methods.getContacts,
+  updateContact: methods.updateContact,
+  hello: methods.hello
 };
 
 // GraphQL middleware
